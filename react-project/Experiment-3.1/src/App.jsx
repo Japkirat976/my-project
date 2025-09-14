a62d87b16695785e165bc978211efdaad1acd9a1
Japkirat976 <japkirat.ladher@gmail.com>
2025-09-14 20:41:45 +0530

    Adding React-project file

---
+++ react-project/Experiment-3.1/src/App.jsx
@@ -0,0 +1,26 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import ProductCard from './ProductCard.jsx'
+
+const products = [
+  { name: 'Wireless Mouse', price: 25.99, inStock: true },
+  { name: 'Keyboard', price: 45.5, inStock: false },
+  { name: 'Monitor', price: 199.99, inStock: true }
+]
+
+function App() {
+  return (
+    <>
+        <h2>Products List</h2>
+        <div style={{ display: 'flex', flexDirection: 'row', gap: '50px' }}>
+          <ProductCard product={products[0]} />
+          <ProductCard product={products[1]} />
+          <ProductCard product={products[2]} />
+        </div>
+    </>
+  )
+}
+
+export default App
